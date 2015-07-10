e9ecc95ebe7bf39bde4a6cbdf35a8b7d9a3260e7
alienAlex <alienAlex@users.noreply.github.com>
2015-07-10 14:10:28 -0400

    implemented segmented control to change units between km and mi. working

--- HowFar/FirstViewController.m
+++ HowFar/FirstViewController.m
@@ -8,28 +8,31 @@
 
 #import "FirstViewController.h"
 #import <MapKit/MapKit.h>
+#define METERS_TO_MILES 0.000621371
 
 @interface FirstViewController () <MKMapViewDelegate>
-
 - (IBAction)clearAllPins:(UIButton *)sender;
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UIButton *clearPinsButton;
 @property (weak, nonatomic) IBOutlet UITextField *distanceTextField;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *mapTypeSegmentedControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *unitsSegmentedControl;
 @property (strong, nonatomic) MKGeodesicPolyline *geodesicPolyLine;
 @property (strong, nonatomic) NSMutableArray *pinsArray;
 @property (strong, nonatomic) MKPointAnnotation *pin;
+- (IBAction)changeUnits:(UISegmentedControl *)sender;
 @end
 
 @implementation FirstViewController
 int pinCounter = 0;
+int tapCounter = 0;
 
 #pragma mark - Lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.mapView.delegate = self;
     self.pinsArray = [[NSMutableArray alloc]init];
-    self.distanceTextField.placeholder = @"0 km";
+    [self setPlaceholderUnits];
     [self.mapTypeSegmentedControl setSelectedSegmentIndex:0]; //Default index is 0 for Standard MapType
     UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc]initWithTarget:self
                                                                                            action:@selector(handleLongPressGesture:)];
@@ -52,18 +55,20 @@ int pinCounter = 0;
         [self.pinsArray addObject:self.pin];
         [self.mapView addAnnotations:self.pinsArray];
         [self displayDistanceBetweenPins];
+        [self changeUnits:nil];
     }
 }
 
 #pragma mark - Miscellaneous
--(void)displayDistanceBetweenPins {
+-(CGFloat)displayDistanceBetweenPins {
+    CGFloat calculatedDistance = 0;
     if([self.pinsArray count] == 2) {
         CLLocation *pointA = [self.pinsArray firstObject];
         CLLocation *A = [[CLLocation alloc]initWithLatitude:pointA.coordinate.latitude longitude:pointA.coordinate.longitude];
         CLLocation *pointB = [self.pinsArray lastObject];
         CLLocation *B = [[CLLocation alloc]initWithLatitude:pointB.coordinate.latitude longitude:pointB.coordinate.longitude];
         CLLocationDistance distance = [B distanceFromLocation:A];
-        self.distanceTextField.text = [NSString stringWithFormat:@"%.2f km", distance / 1000];
+        calculatedDistance = distance;
         //Draws a MKGeodesicPolyline between the two points.
         CLLocationCoordinate2D coords[2] = {A.coordinate, B.coordinate};
         self.geodesicPolyLine = [MKGeodesicPolyline polylineWithCoordinates:coords count:2];
@@ -73,14 +78,19 @@ int pinCounter = 0;
         self.distanceTextField.text = nil;
         //TODO: Don't allow creating a third pin.
     }
+    return calculatedDistance;
 }
 
-- (IBAction)clearAllPins:(UIButton *)sender {
-    [self.mapView removeAnnotations:self.pinsArray];
-    [self.pinsArray removeAllObjects];
-    self.distanceTextField.text = nil;
-    [self.mapView removeOverlay:self.geodesicPolyLine];
-    pinCounter = 0;
+-(void)displayMetricDistance {
+    CGFloat distance = [self displayDistanceBetweenPins];
+    double metricDistance = distance / 1000;
+    self.distanceTextField.text = [NSString stringWithFormat:@"%.2f km", metricDistance];
+}
+
+-(void)displayImperialDistance {
+    CGFloat distance = [self displayDistanceBetweenPins];
+    double imperialDistance = distance * METERS_TO_MILES;
+    self.distanceTextField.text = [NSString stringWithFormat:@"%.2f mi", imperialDistance];
 }
 
 - (IBAction)changeMapType:(UISegmentedControl *)sender {
@@ -97,6 +107,37 @@ int pinCounter = 0;
     }
 }
 
+- (IBAction)changeUnits:(UISegmentedControl *)sender {
+    NSInteger index = [self.unitsSegmentedControl selectedSegmentIndex];
+    if(index == 0) {
+        [self displayMetricDistance];
+    }
+    else {
+        [self displayImperialDistance];
+    }
+}
+
+- (IBAction)clearAllPins:(UIButton *)sender {
+    [self.mapView removeAnnotations:self.pinsArray];
+    [self.pinsArray removeAllObjects];
+    if ([self.mapView.overlays count] > 0) {
+        [self.mapView removeOverlays:[self.mapView overlays]];
+    }
+    self.distanceTextField.text = nil;
+    pinCounter = 0;
+    [self setPlaceholderUnits];
+}
+
+-(void)setPlaceholderUnits {
+    NSInteger index = [self.unitsSegmentedControl selectedSegmentIndex];
+    if(index == 0) {
+        self.distanceTextField.placeholder = @"0.0 km";
+    }
+    else {
+        self.distanceTextField.placeholder = @"0.0 mi";
+    }
+}
+
 #pragma mark - MKMapViewDelegate
 -(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
     if (![overlay isKindOfClass:[MKPolyline class]]) {
@@ -109,5 +150,4 @@ int pinCounter = 0;
     
     return renderer;
 }
-
 @end
